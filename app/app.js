6fca326cdc58b4358dffa9a89bfced0d6f6dad48
mzaini30 <muhzaini30@gmail.com>
2020-02-19 12:31:57 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+anti_cors = 'https://api.allorigins.win/raw?url='
+
 $('.bagian div').css('height', $(window).height() / 2)
-$('.bagian-1').load('https://youtube.com')
-$('.bagian-2').load('https://ytmp3.cc/en13/')
+$('.bagian-1').load(`${anti_cors}https://youtube.com`)
+$('.bagian-2').load(`${anti_cors}https://ytmp3.cc/en13/`)
