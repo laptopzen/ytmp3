4dc7cf603f3c933519ae4405699e5e78644e84b7
mzaini30 <muhzaini30@gmail.com>
2020-02-19 12:08:29 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -1,3 +1,3 @@
 $('.bagian div').css('height', $(window).height() / 2)
-$('.bagian-1').load('https://youtube.com')
-$('.bagian-2').load('https://ytmp3.cc/en13/')
+$('.bagian-1').load('//youtube.com')
+$('.bagian-2').load('//ytmp3.cc/en13/')
